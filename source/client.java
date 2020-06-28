eb1a5b92f3cbcfd6efa0d96b15a1bbf06c9f6ecf
Claxl <claxlxl@gmail.com>
2020-06-28 23:54:32 +0200

    Update client.java

--- source/client.java
+++ source/client.java
@@ -13,7 +13,7 @@ public class ClientChat{
 		String msgRicevuto;
 		try{
 			connessione = new Socket(server, port);
-			System.out.println(“Connessione eseguita.");
+			System.out.println("Connessione eseguita.");
 		}catch(IOException e){
 			System.out.println(e);
 			System.exit(-1);
